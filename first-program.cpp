330f4953c37524422be7b9019fbc64c080db3104
Sujon Hossain <sujon.hossain758@gmail.com>
2021-11-03 23:32:02 +0600

    Yeah I am learning Data Structure and Algorithm

---
+++ first-program.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Setup Ready!" << endl;
+    cout << "Let Move to learning Data Structure and Algorithms" << endl;
+    cout << "Yeah!" << endl;
+    return 0;
+}
